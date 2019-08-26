afac693a88fad3dd7842d296f64303b2de4818c3
Eugene <mrshaftoe@gmail.com>
2019-08-26 20:16:38 +0300

    All board elements rendering in BoardController

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -4,7 +4,13 @@ import {BoardTasks} from "../components/boardtasks";
 import {Task} from "../components/task";
 import {NoTasks} from "../components/notasks";
 import {TaskEdit} from "../components/taskedit";
-import {render} from "../utils";
+import {Sorting} from "../components/sorting";
+import {render, deleteElement} from "../utils";
+import {LoadMoreButton} from "../components/loadmore";
+import {LOAD_MORE_TEXT, NO_TASKS_MESSAGE, SORTINGS} from "../components/data";
+
+const TASKS_SHOWN_ONCE = 8;
+let renderedCardsCount = TASKS_SHOWN_ONCE;
 
 class BoardController {
   constructor(container, tasks) {
@@ -19,7 +25,15 @@ class BoardController {
     render(this._container, this._board.getElement(), `beforeend`);
     render(this._board.getElement(), this._filtersList.getElement(), `beforeend`);
     render(this._board.getElement(), this._boardTasks.getElement(), `beforeend`);
-    this._tasks.forEach((taskMock) => this._renderTask(taskMock));
+
+    if (!this._tasks.length || this._tasks.every(({isArchive}) => isArchive)) {
+      this._renderNoTasks(NO_TASKS_MESSAGE);
+      return;
+    }
+
+    SORTINGS.forEach((sortingMock) => this._renderSorting(sortingMock));
+    this._tasks.slice(0, TASKS_SHOWN_ONCE).forEach((taskMock) => this._renderTask(taskMock));
+    this._renderLoadMoreButton(LOAD_MORE_TEXT);
   }
 
   _renderTask(taskMock) {
@@ -61,6 +75,54 @@ class BoardController {
       .addEventListener(`submit`, closeTaskEdit);
 
     render(this._boardTasks.getElement(), task.getElement(), `beforeend`);
+  }
+
+  _renderSorting(sortingMock) {
+    const sorting = new Sorting(sortingMock);
+    render(
+        this._filtersList.getElement(),
+        sorting.getElement(),
+        `beforeend`
+    );
+  }
+
+  _renderLoadMoreButton(loadMoreMock) {
+    if (renderedCardsCount >= this._tasks.length) {
+      return;
+    }
+
+    const loadMoreButton = new LoadMoreButton(loadMoreMock);
+
+    loadMoreButton.getElement().addEventListener(`click`, (evt) => {
+      evt.preventDefault();
+
+      const startIndex = renderedCardsCount;
+      if (startIndex >= this._tasks.length) {
+        deleteElement(loadMoreButton);
+        return;
+      }
+
+      const endIndex = (startIndex + TASKS_SHOWN_ONCE) < this._tasks.length ? startIndex + TASKS_SHOWN_ONCE : this._tasks.length;
+
+      if (endIndex >= this._tasks.length - 1) {
+        deleteElement(loadMoreButton);
+
+      }
+      this._tasks.slice(startIndex, endIndex).forEach((taskMock) => this._renderTask(taskMock));
+      renderedCardsCount += endIndex - startIndex;
+    });
+
+    render(
+        this._container.querySelector(`.board`),
+        loadMoreButton.getElement(),
+        `beforeend`
+    );
+  }
+
+  _renderNoTasks(noTasksMock) {
+    const noTasksMessage = new NoTasks(noTasksMock);
+    this._board.getElement().innerHTML = ``;
+    render(this._board.getElement(), noTasksMessage.getElement(), `afterbegin`);
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -1,17 +1,13 @@
-import {getTaskData, getFiltersData, CONTROLS, SEARCH_PLACEHOLDER, SORTINGS, LOAD_MORE_TEXT, NO_TASKS_MESSAGE} from './components/data.js';
+import {getTaskData, getFiltersData, CONTROLS, SEARCH_PLACEHOLDER} from './components/data.js';
 import {Control} from './components/control';
 import {Search} from './components/search';
 import {Filter} from './components/filters';
-import {LoadMoreButton} from './components/loadmore';
-import {Sorting} from './components/sorting';
-import {NoTasks} from './components/notasks';
-import {render, unrender, createElement, deleteElement} from './utils';
+import {render, createElement} from './utils';
 import {BoardController} from './controllers/board.js';
 
-const CARD_SHOWN_ONCE = 8;
 const main = document.querySelector(`.main`);
 const tasks = Array.from({length: 30}, getTaskData);
-let renderedCardsCount = CARD_SHOWN_ONCE;
+
 
 const renderControl = function (controlMock) {
   const control = new Control(controlMock);
@@ -40,55 +36,6 @@ const renderFilter = function (filterMock) {
   );
 };
 
-const renderSorting = function (sortingMock) {
-  const sorting = new Sorting(sortingMock);
-  render(
-      main.querySelector(`.board__filter-list`),
-      sorting.getElement(),
-      `beforeend`
-  );
-};
-
-const renderLoadMoreButton = function (loadMoreMock) {
-  if (renderedCardsCount >= tasks.length) {
-    return;
-  }
-
-  const loadMoreButton = new LoadMoreButton(loadMoreMock);
-
-  loadMoreButton.getElement().addEventListener(`click`, function (evt) {
-    evt.preventDefault();
-
-    const startIndex = renderedCardsCount;
-    if (startIndex >= tasks.length) {
-      unrender(loadMoreButton.getElement());
-      return;
-    }
-
-    const endIndex = (startIndex + CARD_SHOWN_ONCE) < tasks.length ? startIndex + CARD_SHOWN_ONCE : tasks.length;
-
-    if (endIndex >= tasks.length - 1) {
-      deleteElement(loadMoreButton);
-
-    }
-    tasks.slice(startIndex, endIndex).forEach(renderTask);
-    renderedCardsCount += endIndex - startIndex;
-  });
-
-  render(
-      main.querySelector(`.board`),
-      loadMoreButton.getElement(),
-      `beforeend`
-  );
-};
-
-const renderNoTasks = function (noTasksMock) {
-  const noTasksMessage = new NoTasks(noTasksMock);
-  const board = main.querySelector(`.board`);
-  board.innerHTML = ``;
-  render(board, noTasksMessage.getElement(), `afterbegin`);
-};
-
 render(
     main.querySelector(`.main__control`),
     createElement(`<section class="control__btn-wrap"></section>`),
@@ -107,12 +54,4 @@ getFiltersData(tasks).forEach(renderFilter);
 
 const boardController = new BoardController(main, tasks);
 boardController.init();
-
-// if (!tasks.length || tasks.every(({isArchive}) => isArchive)) {
-//   renderNoTasks(NO_TASKS_MESSAGE);
-// } else {
-//   SORTINGS.forEach(renderSorting);
-//   renderLoadMoreButton(LOAD_MORE_TEXT);
-//   tasks.slice(0, CARD_SHOWN_ONCE).forEach(renderTask);
-// }
 
